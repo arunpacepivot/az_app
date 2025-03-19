0896499f939e867d1bcc315016f2bef90e9e5c7a
Arpit-pacepivot <arpit@pacepivot.com>
2025-03-20 00:20:19 +0530

    fixed sp backend calling issue

--- frontend/src/app/sp/page.tsx
+++ frontend/src/app/sp/page.tsx
@@ -26,7 +26,9 @@ export default function ListingGeneratorForm() {
   const [outputFile, setOutputFile] = useState<Blob | null>(null)
 
   // Use environment variables for the backend URL
-  const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000/"
+  const isDevelopment = process.env.NODE_ENV === 'development';
+    const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 
+      (isDevelopment ? "http://localhost:8000/" : "https://django-backend-epcse2awb3cyh5e8.centralindia-01.azurewebsites.net/");
 
   const features = [
     {
